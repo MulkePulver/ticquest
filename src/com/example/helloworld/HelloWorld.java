8f0464b73999f951aeb626ab155132f13ff582fa
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-29 21:33:58 -0500

    Commit de prueba para trabajar con Java desde sede.

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+//Al quitarle cuatro años a la edad de Hugo se obtiene dos veces la edad de Paco y si sumamos las edades de Hugo y Paco se obtiene cinco veces la edad de Luis (todos en años enteros). En el país del Sagrado Corazón de Jesús se dice que una persona está en etapa 'uno' si está entre 0 y 20 años, en etapa 'dos' si está entre 21 y 30 años, en etapa 'tres si está entre 31 y 50 años y en etapa 'cuatro' si está por encima de 50 años. Dada la edad de Paco, realizar  un programa que imprima en la primera línea las edades de Paco, Hugo y Luis separadas por un solo espacio y en la segunda línea indique en qué etapa se encuentra Luis.
+
+
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello world");
+    }
+}
