6459e0062e372b8f9e7cea22f230dea33edcacef
mulkepulv <juanandres.garcia.duarte@gmail.com>
2021-07-29 21:51:47 -0500

    Se añaden archivos de prueba.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@
 
 package com.example.helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello world");
---
+++ src/com/example/helloworld/valorAbsoluto.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class valorAbsoluto {
+    public class Main {
+        public static double valorAbsoluto(double x) {
+            double valor;
+            if (x >= 0) {
+                valor = x;
+            } else {
+                valor = -x;
+            }
+            return  valor;
+        }
+    }
+    System.out.print("")
+}
